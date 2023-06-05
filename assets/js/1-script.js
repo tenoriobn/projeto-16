c065fba4896c635f9bfe9f77edcfa5b8e6e992a4
Bruno Tenório <tenoriobn@gmail.com>
2023-06-05 11:52:17 -0300

    Finalizando evento de troca de tema Dark/Light

--- assets/js/1-script.js
+++ assets/js/1-script.js
@@ -1,9 +1,10 @@
+/* Aplicando Evento de click para transformar tema Dark em Light */
 const changeTheme = document.querySelector(".checkbox");
 
 changeTheme.addEventListener("click", () => {
-    const body = document.querySelector("body");
-    const header = document.querySelector("header");
+    const elements = document.querySelectorAll("body, header, .header__branding, .header__title, .header__followers, .header__dark-mode-label, .header__dark-mode-switch, .checkbox, .card, .card__username, .card__followers, .card__followers-label, .overview-section__title, .card__label, .card__value");
 
-    body.classList.toggle("light")
-    header.classList.toggle("light")
+    Array.from(elements).forEach((element) => {
+        element.classList.toggle("light");
+    })
 })
