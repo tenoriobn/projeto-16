0b2eef66e74d339c987a53b52da34fb4ef494737
Bruno Tenório <tenoriobn@gmail.com>
2023-06-06 17:54:12 -0300

    Criando cards dinamicos com dados json

--- assets/js/1-script.js
+++ assets/js/1-script.js
@@ -9,6 +9,63 @@ changeTheme.addEventListener("click", () => {
     })
 })
 
+function createCard() {
+    const cardSection = document.querySelector(".card-section");
+
+    fetch('./assets/js/card-section.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(item => {
+            const { title, info: { username, followers, count } } = item;
+
+            const article = document.createElement('article');
+            article.classList.add('card', title.toLowerCase());
+
+            const cardInfo = document.createElement('div');
+            cardInfo.classList.add('card__info')
+
+            const cardIcon = document.createElement('span');
+            cardIcon.classList.add('card__icon', item.info.iconsocial);
+
+            const cardUserName = document.createElement('p');
+            cardUserName.classList.add('card__username')
+            cardUserName.textContent = `${username}`;
+
+            const cardFollowers = document.createElement('h3');
+            cardFollowers.classList.add('card__followers');
+            cardFollowers.textContent = `${followers}`;
+
+            const CardfollowersLabel = document.createElement('p');
+            CardfollowersLabel.classList.add('card__followers-label');
+            CardfollowersLabel.textContent = "followers";
+
+            const cardToday = document.createElement('div');
+            cardToday.classList.add('card__today');
+
+            const cardIconCount = document.createElement('span');
+            cardIconCount.classList.add('card__icon', item.info.iconcount);
+
+            const cardTodayCount = document.createElement('h3');
+            cardTodayCount.classList.add('card__today-count', item.info.countcolor);
+            cardTodayCount.textContent = `${count} Today`;
+
+            cardSection.appendChild(article);
+            article.appendChild(cardInfo);
+
+            cardInfo.appendChild(cardIcon);
+            cardInfo.appendChild(cardUserName);
+
+            article.appendChild(cardFollowers);
+            article.appendChild(CardfollowersLabel);
+
+            article.appendChild(cardToday);
+            cardToday.appendChild(cardIconCount);
+            cardToday.appendChild(cardTodayCount);
+        })
+    })
+}
+
+createCard();
 
 /*
     ANTES DE PROSSEGUIR COM O RESTO O IDIEL É APLICAR O EVENTO DE DARK/LIGHT
